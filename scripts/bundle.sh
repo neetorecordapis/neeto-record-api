1ee5a7d0b0d4dc7b4f6231fe5d4a15551744b20b
S Varun <unnivarun99@gmail.com>
2026-03-31 17:34:22 +0530

    Updated v2 api endpoint documentation

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -1,1 +1,2 @@
 find ./docs -name "*.yaml" -type f -exec sh -c './node_modules/.bin/redocly bundle "$1" --output "bundled/$(basename "$1" .yaml).yaml"' _ {} \;
+find ./docs-v1 -name "*.yaml" -type f -exec sh -c './node_modules/.bin/redocly bundle "$1" --output "bundled-v1/$(basename "$1" .yaml).yaml"' _ {} \;
